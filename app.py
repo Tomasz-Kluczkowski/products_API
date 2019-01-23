f3574ea12b63b88d809b63e05aedb90c41592c79
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2019-01-23 10:55:37 +0000

    Add product resource to the api.

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from flask_restful import Api
 
 from resources.user import UserResource
+from resources.product import ProductResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 api.add_resource(UserResource, '/user')
+api.add_resource(ProductResource, '/product')

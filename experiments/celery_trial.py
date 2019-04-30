5c205b424e718b51a5d6b393caade11d8f9a4314
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-05-01 00:12:03 +0100

    Celery trial.

---
+++ experiments/celery_trial.py
@@ -0,0 +1,35 @@
+from celery import Celery
+from devtools import debug
+
+import time
+
+celery_app = Celery('celery_trial', backend='rpc://', broker='pyamqp://guest@localhost//')
+
+
+class BaseTask(celery_app.Task):
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        debug(task_id)
+        debug(einfo)
+        debug(args)
+        debug(kwargs)
+
+
+@celery_app.task(base=BaseTask, bind=True, name='experiments.celery_trial.add')
+def add(self, x, y):
+    debug('CALCULATING')
+    try:
+        raise Exception
+        return x + y
+    except Exception as exc:
+        self.retry(exc=exc, max_retries=2, countdown=2)
+
+
+# result = add.delay(2, 1)
+
+
+# debug(result)
+# debug(result.status)
+# debug(result.state)
+# status = result.get()
+# debug(status)

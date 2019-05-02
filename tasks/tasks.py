71195c087c21fbbc4290ceaca2ad5e1f347dc20d
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-05-02 22:48:24 +0100

    Add on_failure.

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -4,7 +4,20 @@ from database.models import User
 from run import celery
 
 
-@celery.task
-def print_hello():
+class PrintTask(celery.Task):
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        debug('FAILED!!!!')
+        debug(self.app)
+        # with self.app.app_context():
+        # self.app
+        # with app.app_context():
+        #     users = User.query.all()
+        # debug(users)
+
+
+@celery.task(bind=True, base=PrintTask, autoretry_for=(Exception, ), retry_kwargs={'max_retries': 2, 'countdown': 2})
+def print_hello(self):
+    debug('TRIGGERING EXCEPTION')
     users = User.query.all()
     debug(users)
+    raise Exception
